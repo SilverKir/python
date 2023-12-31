75bb0674b990ba42ffd8908ef2edf78e3522626a
silver_kir <rci@mp03.ru>
2023-12-31 12:36:41 +0800

    bug fixed

--- Task50.py
+++ Task50.py
@@ -17,13 +17,13 @@
 
 # Функции (можно вынести в отдельный файл) можно доработать что бы словарь не ограничивалс 3 строками
 
-def print_menu(list_print,argument=None):
+def print_menu(list_print, argument=None):
         global contact_id
         print(list_print[0][0])
-        contact_id=argument #Индекс выбранного адреса
-        if list_print[0][1]:exec(list_print[0][1]) # если есть команда у меню
+        contact_id = argument #Индекс выбранного адреса
+        if list_print[0][1]: exec(list_print[0][1]) # если есть команда у меню
         [print(f"{i}. {list_print[i][0]}") for i in range(len(list_print)) if i>0]
-        menu_item= int(input("Введите нужный номер: "))
+        menu_item = int(input("Введите нужный номер: "))
         while menu_item not in range(1,len(list_print)):
                 menu_item=int(input(f"Ваше значение не верно, введите число в диапазоне от 1 до {len(list_print) - 1}: "))
         else:
@@ -35,13 +35,18 @@ def save_file(file_name, contact_list):
         file.close()
         print(f"Данные успешно сохранены в файл {file_name}")
 def open_file(file_name):
-        with open(file_name,"r") as file:
-                data_tel=file.readlines()
-                tel_cont =[]
-                [tel_cont.append(tel_item.split(";")) for tel_item in data_tel]
-                for i in range(len(tel_cont)):
-                        tel_cont[i][-1]=tel_cont[i][-1].replace("\n","")
-        file.close()
+        tel_cont = []
+        try:
+                with open(file_name,"r") as file:
+                        data_tel=file.readlines()
+                        [tel_cont.append(tel_item.split(";")) for tel_item in data_tel]
+                        for i in range(len(tel_cont)):
+                                tel_cont[i][-1]=tel_cont[i][-1].replace("\n","")
+                file.close()
+        except IOError:
+                file=open(file_name, "w+")
+                file.close()
+
         return tel_cont
 
 def new_contact(list_contacts):
@@ -52,10 +57,14 @@ def new_contact(list_contacts):
         exec(print_menu(main_menu))
 
 def print_contacts(list_contacts,param=1):
-        [print(f"{i+1}. Имя:{list_contacts[i][0]} Телефон:{list_contacts[i][1]} Ник:{list_contacts[i][2]}") for i in range(len(list_contacts))]
-        if param==1:
-                input()
-                exec(print_menu(main_menu))
+        if len(list_contacts)==0:
+                print("Телефонная книга пуста: Занесите пожалуйста первый контакт:")
+                new_contact(list_contacts)
+        else:
+                [print(f"{i+1}. Имя:{list_contacts[i][0]} Телефон:{list_contacts[i][1]} Ник:{list_contacts[i][2]}") for i in range(len(list_contacts))]
+                if param==1:
+                        input()
+                        exec(print_menu(main_menu))
 def print_contact_item(list_contacts,contact_id):
         print(f"Контакт Имя:{list_contacts[contact_id][0]} Телефон:{list_contacts[contact_id][1]} Ник:{list_contacts[contact_id][2]}")
 
@@ -69,7 +78,7 @@ def edit_contact_item(list_contacts,contact_id):
 
 def del_contact_item(list_contacts,contact_id,type_del=1):
         if type_del==1:
-                return exec(print_menu(list_del))
+                return exec(print_menu(list_del, contact_id))
         else:
                 print_contact_item(list_contacts, contact_id)
                 list_contacts.pop(contact_id)
@@ -77,35 +86,39 @@ def del_contact_item(list_contacts,contact_id,type_del=1):
                 print_contacts(tel_contact)
 
 def find_contact(list_contacts, find_type):
-        if find_type==1: # Поиск по индексу
-                print_contacts(list_contacts, 2)
-                menu_item = int(input("Введите нужный номер: "))
-                while menu_item not in range(1, len(list_contacts)+1):
-                        menu_item = int(input(f"Ваше значение не верно, введите число в диапазоне от 1 до {len(list_contacts)}: "))
-                else:
-                        return exec(print_menu(list_edit, int(menu_item)-1))
-        elif find_type==2: # Поиск по имени
-                find_criterion=input("Введите имя для поиска:")
-                for item in list_contacts:
-                        if find_criterion in item[0]:
-                              return exec(print_menu(list_edit,list_contacts.index(item)))
-        elif find_type == 3:  # Поиск по телефону
-                find_criterion = input("Введите телефон для поиска:")
-                for item in list_contacts:
-                        if find_criterion in item[1]:
-                                return exec(print_menu(list_edit, list_contacts.index(item)))
-        elif find_type == 4:  # Поиск по нику
-                find_criterion = input("Введите ник для поиска:")
-                for item in list_contacts:
-                        if find_criterion in item[2]:
-                                return exec(print_menu(list_edit, list_contacts.index(item)))
-        print("К сожалению, поиск не увенчался успехом, нажмите Enter для выхода в главное меню")
-        input()
-        exec(print_menu(main_menu))
+        if len(list_contacts)==0:
+                print("Телефонная книга пуста: Занесите пожалуйста первый контакт:")
+                new_contact(list_contacts)
+        else:
+                if find_type==1: # Поиск по индексу
+                        print_contacts(list_contacts, 2)
+                        menu_item = int(input("Введите нужный номер: "))
+                        while menu_item not in range(1, len(list_contacts)+1):
+                                menu_item = int(input(f"Ваше значение не верно, введите число в диапазоне от 1 до {len(list_contacts)}: "))
+                        else:
+                                return exec(print_menu(list_edit, int(menu_item)-1))
+                elif find_type==2: # Поиск по имени
+                        find_criterion=input("Введите имя для поиска:")
+                        for item in list_contacts:
+                                if find_criterion in item[0]:
+                                      return exec(print_menu(list_edit,list_contacts.index(item)))
+                elif find_type == 3:  # Поиск по телефону
+                        find_criterion = input("Введите телефон для поиска:")
+                        for item in list_contacts:
+                                if find_criterion in item[1]:
+                                        return exec(print_menu(list_edit, list_contacts.index(item)))
+                elif find_type == 4:  # Поиск по нику
+                        find_criterion = input("Введите ник для поиска:")
+                        for item in list_contacts:
+                                if find_criterion in item[2]:
+                                        return exec(print_menu(list_edit, list_contacts.index(item)))
+                print("К сожалению, поиск не увенчался успехом, нажмите Enter для выхода в главное меню")
+                input()
+                exec(print_menu(main_menu))
 
 #   Программа, задает меню и тд.
 
-list_del=[("           УДАЛЕНИЕ \nВы точно хотите удалить запись: ","print_contact_item(tel_contact,contact_id)"),("NO","exec(print_menu(list_edit))"),("YES","del_contact_item(tel_contact,contact_id,2)"),("Вернуться в главное меню","exec(print_menu(main_menu))" )]
+list_del=[("           УДАЛЕНИЕ \nВы точно хотите удалить запись: ","print_contact_item(tel_contact,contact_id)"),("NO","exec(print_menu(list_edit,contact_id))"),("YES","del_contact_item(tel_contact,contact_id,2)"),("Вернуться в главное меню","exec(print_menu(main_menu))" )]
 list_edit=[("           РЕДАКТИРОВАНИЕ \nДействия с выбранной записью","print_contact_item(tel_contact,contact_id)"),("Edit","edit_contact_item(tel_contact,contact_id)"),("Delet","del_contact_item(tel_contact,contact_id)"),("Вернуться в главное меню","exec(print_menu(main_menu))" )]
 list_exit=[("           ВЫХОД \nСохранить изменения?",""),("YES","save_file(file_name,tel_contact)"),("NO","print("'"До свидания! Вышли без сохранения."'")"),("Вернуться в главное меню","exec(print_menu(main_menu))" )]
 main_menu=[("           ГЛАВНОЕ МЕНЮ \nВыберете Ваше действие:",""),("Show contacts","print_contacts(tel_contact)"),("Create contact","new_contact(tel_contact)"),("Find (Edit, Delete)","exec(print_menu(list_find))"),("Exit","exec(print_menu(list_exit))")]
